2674dd9df0d7e35a92ddd0e62926ab776edf47ec
Ashraf <ashrafmohamud@gmail.com>
2020-06-03 19:00:30 +0100

    tweaked file slightly for more readability

--- fibonacci_game.py
+++ fibonacci_game.py
@@ -1,9 +1,9 @@
 fibonacci = [0, 1]
 stop_value = int(input("Where should we stop the sequence? "))
-#1 create while loop = while true
+# created a while loop = while true
 while True:
-#2 initiate fibonacci list with 1 and 1
-    # fibonacci = [0, 1, 1] -> I had to take this initiation outside the loop so it doesnt re-set every time it loops.
+# initiate fibonacci list with 1 and 1
+    # fibonacci = [0, 1, 1] -> I had to take this initiation outside the loop so it doesnt reset every time it loops.
 #3 append list[-2] + list[-1]
     fibonacci.append(fibonacci[-2] + fibonacci[-1])
 #4 print list and keep looping
@@ -11,3 +11,4 @@ while True:
 
     if fibonacci[-1] > stop_value:
         break
+# when the sequence reaches number you said break
